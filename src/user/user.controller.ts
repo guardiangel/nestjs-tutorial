b673a1ca34dd8057bd3cf3616312798687182696
guardiangel <felix.sgq@gmail.com>
2023-11-12 20:11:37 -0500

    Customize token guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   // GET /users/me
-  @UseGuards(AuthGuard('jwttoken'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
