b33b96f3c7542ec30d956c1b9eff98cab7df0c49
guardiangel <felix.sgq@gmail.com>
2023-11-12 20:24:55 -0500

    Transfer @Req using decorator pattern for scalability

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 
-import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   // GET /users/me
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
